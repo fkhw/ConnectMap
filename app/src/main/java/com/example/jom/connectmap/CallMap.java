2a2237da452e91bb7df4b67909b522d6c74cfb73
fkhw <fkhw.s@hotmail.com>
2017-04-20 11:48:01 +0700

    Test Map

--- app/src/main/java/com/example/jom/connectmap/CallMap.java
+++ app/src/main/java/com/example/jom/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package com.example.jom.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class CallMap extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_map);
     }//OnCreate Method
+
+    public void runCallMap (View view){
+
+        //Initial view
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?q=loc:13.759847,100.541554(ศุนย์รางน้ำ)");
+
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW,location);
+        mapIntent.setPackage("com.google.android.apps.maps"); //ใช้ไม่ได้กับ Genymotion ใช้ได้กับ Android จริงๆ
+        startActivity(mapIntent);
+
+    }//runCallMap
 
 }//Main Class
